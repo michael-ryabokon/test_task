bf8abb662e9b94bd543958d23aeed1e0a3fc2dfd
michael-ryabokon <michael.ryabokon@gmail.com>
2014-05-29 17:27:37 +0300

    crappy animation in both way

--- js/slide.js
+++ js/slide.js
@@ -51,70 +51,93 @@ var Slide = Class.create(Animation, {
 
 	onMouseDown: function (event) {
 		this.startX = event.clientX;
-		this.startSwipe = true;
+		this.swipeStart = true;
 
 	},
 
 	onMouseMove: function (event) {
-		if (this.startSwipe) {
-			this.direction = ((event.clientX - this.startX) > 0) ? 1 : -1; // -1 -> swipe to left; 1 -> swipe to right
+		if (!this.swipeStart) return;
 
-			this.swipe = Math.abs(event.clientX - this.startX);
+		this.direction = ((event.clientX - this.startX) > 0) ? 1 : -1; // -1 -> swipe to left; 1 -> swipe to right
 
-			if (!this.currentImage.previousElementSibling) {
-				this.currentImage.style.marginLeft = (this.swipe * this.direction) + 'px';	
-			} else {
-				this.currentImage = this.currentImage[this.siblings[this.direction]];
-				this.currentImage.style.webkitTransitionDuration = (500 / 1000) + 's';
-				this.currentImage.style.marginLeft = 0;
+		this.swipe = Math.abs(event.clientX - this.startX);
 
-				// (this.swipe * this.direction) + 'px';
-			}
+		if (this.direction > 0) {
+			this.previousImage = 
+				this.currentImage.previousElementSibling || 
+				this.prepend(this.getElement('last')) ||
+				this.currentImage.previousElementSibling;
 
-			
-			
-			console.log('move');
+			this.previousImage.style.marginLeft = 
+				- this.previousImage.offsetWidth + (this.swipe * this.direction) + 'px';
+		} else {
+			this.nextImage = 
+				this.currentImage.nextElementSibling ||
+				this.append(this.getElement('first')) || 
+				this.currentImage.nextElementSibling;
+
+			this.currentImage.style.marginLeft = (this.swipe * this.direction) + 'px';
+
 		}
+			
+		console.log('move');
+
 	},
 
 	onMouseUp: function (event) {
-		this.currentImage.style.webkitTransitionDuration = (500 / 1000) + 's';
+		this.currentImage.style.webkitTransitionDuration = this.getDuration();
+		if (this.previousImage) {
+			this.previousImage.style.webkitTransitionDuration = this.getDuration(); 
+		}
 
-		if (this.swipe > 100) {
-			this.currentImage.style.marginLeft = 
-			(parseInt(this.currentImage.style.width) * this.direction) + 'px';
+		if (this.swipe > 50) {
+////////// to left
+			if (this.direction > 0) {
+				this.previousImage.style.webkitTransitionDuration = this.getDuration();
+				this.previousImage.style.marginLeft = 0;
+
+				setTimeout(function () {
+					this.currentImage = this.currentImage[this.siblings[this.direction]];
+					this.currentImage.nextElementSibling.style.webkitTransitionDuration = '0s';
+				}.bind(this), 500);
+			} else {
+////////// to right
+				this.currentImage.style.marginLeft = 
+					(this.currentImage.offsetWidth * this.direction) + 'px';			
 
 				setTimeout(function () {
 					this.currentImage.style.webkitTransitionDuration = '0s';
 					this.currentImage = this.currentImage[this.siblings[this.direction]];
-				}.bind(this), 500);
-
-				/*setTimeout(function () {
-					this.currentImage.style.webkitTransitionDuration = '0s';
-					this.currentImage = this.currentImage[this.siblings[this.direction]];
-					this.replaceElements();
-				}.bind(this), 500); //change to animation time*/
+				}.bind(this), 500); //change to animation time
+			}
 
 		} else {
+			if (this.previousImage) { 
+				this.previousImage.style.marginLeft =  
+					(- this.previousImage.offsetWidth) + 'px'; 
+				setTimeout(function () {
+					this.previousImage.style.webkitTransitionDuration = '0s';
+				}.bind(this), 500);
+			}
+
+			setTimeout(function () {
+					this.currentImage.style.webkitTransitionDuration = '0s';
+				}.bind(this), 500);
+
 			this.currentImage.style.marginLeft = '0';
 		}
 
 		this.swipe = 0;
 
 		console.log('up');
-		this.startSwipe = false;
+		this.swipeStart = false;
 	},
-/*
-	getFirstImage: function () {
-		return document.getElementsByClassName('image').item(0);
-	},*/
+
+	getDuration: function () {
+		return (500 / 1000) + 's'; //change to animation options;
+	},
 
 	getElement: function (place) {
-		/*var images = document.getElementsByClassName('image'),
-				index = (place === 'first') ? 0 : image.length - 1;
-
-		return images.item(index);*/
-
 		return this.container[place + 'Child'];
 	},
 
@@ -132,11 +155,17 @@ var Slide = Class.create(Animation, {
 	},
 
 	append: function (element) {
-			this.container.appendChild(element.remove());
+			var el = element.remove();
+
+			el.style.marginLeft = '0px';
+
+			this.container.appendChild(el);
 	},
 
 	prepend: function (element) {
 		this.container.insertBefore(element.remove(), this.getElement('first'));
+
+		return false;
 	}
 
 /*
